88a1f38bd9d9c79c299677aa11b0853672c21d71
RAM-VISHAL <ramvishalgonekar@gmail.com>
2025-12-19 11:26:02 +0530

    [Ramvishal]Adding : programming element level1

---
+++ gcr-codebase/programming-elements/level1/DivideThreePen.java
@@ -0,0 +1,12 @@
+public class DivideThreePen{
+     
+      public static void main(String args[]){
+        int pens = 14;
+        int stud = 3;
+       
+       int division = pens/stud;
+       int remainder = pens%stud;
+
+       System.out.println("The Pen Per Student is "+  division  +"and the remaining pen not distributed is "+ remainder);
+}}
+       
