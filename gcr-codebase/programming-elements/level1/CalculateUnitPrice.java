91e878336df6da16be0f45c6621ba1be705469b1
RAM-VISHAL <ramvishalgonekar@gmail.com>
2025-12-19 11:23:36 +0530

    [Ramvishal] Adding: programming element level1

---
+++ gcr-codebase/programming-elements/level1/CalculateUnitPrice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CalculateUnitPrice{
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int unitPrice = sc.nextInt();
+    int quantity = sc.nextInt();
+    
+    int purchasePrice = unitPrice*quantity;
+  
+    System.out.println("The total purchase price is INR "+ purchasePrice+ " if the quantity "+ quantity+ " and unit price is INR "+ unitPrice);
+}}
