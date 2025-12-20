285b0b90576c29fc40eada2a92712d69fbf9939f
RAM-VISHAL <ramvishalgonekar@gmail.com>
2025-12-20 09:55:48 +0530

    [Ramvishal] Adding :control flow3

---
+++ gcr-codebase/control-flow/level3/BMICalculator.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class BMICalculator{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //  Input weight and height
+        System.out.print("Enter weight in kg: ");
+        double weight = sc.nextDouble();
+
+        System.out.print("Enter height in cm: ");
+        double heightCm = sc.nextDouble();
+
+        // Convert height from cm to meters
+        double heightM = heightCm / 100;
+
+        //  Calculate BMI
+        double bmi = weight / (heightM * heightM);
+
+        //  Determine BMI status
+        String status;
+
+        if (bmi <= 18.4) {
+            status = "Underweight";
+        } else if (bmi <= 24.9) {
+            status = "Normal";
+        } else if (bmi <= 39.9) {
+            status = "Overweight";
+        } else {
+            status = "Obese";
+        }
+
+        //  result
+     
+        System.out.println("BMI Value : " + bmi);
+        System.out.println("Status    : " + status);
+
+        sc.close();
+    }
+}
