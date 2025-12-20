f498ce7c02649e9ec7167372fbe6f1065970db7a
RAM-VISHAL <ramvishalgonekar@gmail.com>
2025-12-20 11:04:36 +0530

    [Ramvishal] Adding :control flow level2

---
+++ gcr-codebase/control-flow/level2/FizzBuzz.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        // Check if input is a positive integer
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer greater than 0.");
+        } else {
+            // Loop from 1 to the entered number
+            for (int i = 1; i <= number; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
+
+
+
+
