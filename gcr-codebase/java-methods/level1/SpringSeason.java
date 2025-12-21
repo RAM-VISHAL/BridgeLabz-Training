31346993eb1828014252ddc317ce99b222351ce7
RAM-VISHAL <ramvishalgonekar@gmail.com>
2025-12-21 23:44:02 +0530

    [Ramvishal] Added : java-method level1

---
+++ gcr-codebase/java-methods/level1/SpringSeason.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class SpringSeason{
+   public static boolean checkSpringSeason(int month,int day){
+     //spring season march 20 to June 20
+
+     if((month == 3 && day >=20) || month == 4 || month == 5 || (month == 6 && day <= 20) ){
+        return true;    
+     }
+     else{
+         return false;
+      }
+     } 
+   
+   public static void main(String args[]){
+     
+      int month =Integer.parseInt(args[0]);
+       int day  = Integer.parseInt(args[1]);
+       
+      // calling method 
+     if(checkSpringSeason(month,day)){
+       System.out.println("Its a Spring Season ");
+       }
+      else{
+       System.out.println("Not a Spring Season ");
+       }
+      
+     }
+    }
