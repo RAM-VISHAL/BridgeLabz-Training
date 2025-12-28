4da809b3d74b5b1a236328a05778a13b90a5c1f7
RAM-VISHAL <ramvishalgonekar@gmail.com>
2025-12-29 00:45:11 +0530

    [Ramvishal]: Added circle class file

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructor/level1/Circle.java
@@ -0,0 +1,21 @@
+package com.constructor.level1;
+
+public class Circle {
+    private double radius;
+
+    // Default Constructor (calls parameterized constructor)
+    public Circle() {
+        this(1.0);   // default radius
+    }
+
+    // Parameterized Constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Optional: Display Method
+    public void display() {
+        System.out.println("Radius: " + radius);
+        
+    }
+}
