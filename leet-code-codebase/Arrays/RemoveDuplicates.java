8b12bc9ce18c70e71b22393ccfaf82297f5e5bfd
RAM-VISHAL <ramvishalgonekar@gmail.com>
2025-12-29 10:34:31 +0530

    [Ramvishal]: Remove duplicate elements from array program

---
+++ leet-code-codebase/Arrays/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+public class RemoveDuplicates{
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for (final int num : nums)
+        if (i < 1 || num > nums[i - 1])
+            nums[i++] = num;
+
+        return i;
+    }
+}
