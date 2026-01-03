3798510748bc8d6cfba69468474296ea633e22ce
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-01-03 06:40:22 +0530

    [Ramvishal]: added program of library management using single inheritance

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/singleinheritance/library/Author.java
@@ -0,0 +1,21 @@
+package com.inheritance.singleinheritance.library;
+
+public class Author extends Book {
+	String name;
+	String bio;
+	
+	Author(String title,int publicationYear,String name,String bio){
+		super(title,publicationYear);
+		this.name = name;
+		this.bio = bio;
+		
+	}
+	
+	void displayInfo() {
+		super.displayInfo();
+		System.out.println(" Author Name "+name);
+		System.out.println("Author Bio "+bio);
+		
+	}
+
+}
---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/inheritance/singleinheritance/library/BookMain.java
@@ -0,0 +1,12 @@
+package com.inheritance.singleinheritance.library;
+
+public class BookMain {
+	
+	public static void main(String args[]) {
+		  Book author = new Author("The Alchemist", 1988, "Paulo Coelho","Brazilian author known for spiritual novels");
+	        
+	       author.displayInfo();
+		
+	}
+
+}
