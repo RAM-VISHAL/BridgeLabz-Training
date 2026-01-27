32517f6318a2fade20dd9c14a637ee12428496fe
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-01-27 10:11:48 +0530

    [Ramvishal]:added program to make custom exception

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/exceptions/customexception/CustomExceptionExample.java
@@ -0,0 +1,34 @@
+package com.exceptions.customexception;
+
+import java.util.Scanner;
+
+//Custom Exception (not public)
+class InvalidAgeException extends Exception {
+
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
+
+public class CustomExceptionExample {
+	public static void validateAge(int age) throws InvalidAgeException {
+		if (age < 18) {
+			throw new InvalidAgeException("Age must be 18 or above");
+		}
+	}
+
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	
+	    try {
+	        System.out.print("Enter age: ");
+	        int age = scanner.nextInt();
+	        validateAge(age);
+	        System.out.println("Access granted!");
+	
+	    } catch (InvalidAgeException e) {
+	        System.out.println(e.getMessage());
+	    }
+	    scanner.close();
+	}
+}
