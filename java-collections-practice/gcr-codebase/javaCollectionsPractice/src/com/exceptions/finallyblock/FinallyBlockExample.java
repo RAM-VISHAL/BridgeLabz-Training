1daa80d3838ce3d0ec6c894473b58c053cb2bc1c
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-01-27 10:13:08 +0530

    [Ramvishal]:added program of implementation of finally block

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/exceptions/finallyblock/FinallyBlockExample.java
@@ -0,0 +1,26 @@
+package com.exceptions.finallyblock;
+
+import java.util.Scanner;
+public class FinallyBlockExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter numerator: ");
+            int a = scanner.nextInt();
+
+            System.out.print("Enter denominator: ");
+            int b = scanner.nextInt();
+
+            int result = a / b;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+
+        } finally {
+            System.out.println("Operation completed");
+            scanner.close();
+        }
+    }
+}
