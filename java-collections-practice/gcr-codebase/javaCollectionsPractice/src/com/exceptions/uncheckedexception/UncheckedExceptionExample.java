336a24664d89004b74a276bb5e85ae30ad69765a
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-01-27 10:16:32 +0530

    [Ramvishal]:added program of unchecked exception

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/exceptions/uncheckedexception/UncheckedExceptionExample.java
@@ -0,0 +1,30 @@
+package com.exceptions.uncheckedexception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UncheckedExceptionExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter numerator: ");
+            int numerator = scanner.nextInt();
+
+            System.out.print("Enter denominator: ");
+            int denominator = scanner.nextInt();
+
+            int result = numerator / denominator;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter valid numeric values");
+
+        } finally {
+            scanner.close();
+        }
+    }
+}
