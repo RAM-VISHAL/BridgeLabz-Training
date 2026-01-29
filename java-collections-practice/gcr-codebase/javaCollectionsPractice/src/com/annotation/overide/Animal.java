eaa5879dae6351dcca2b243c63fc266812bf4adc
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-01-29 08:38:37 +0530

    [Ramvishal] : added program to implement override annotation

---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/annotation/overide/Animal.java
@@ -0,0 +1,7 @@
+package com.annotation.overide;
+
+class Animal {
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
---
+++ java-collections-practice/gcr-codebase/javaCollectionsPractice/src/com/annotation/overide/Main.java
@@ -0,0 +1,8 @@
+package com.annotation.overide;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
