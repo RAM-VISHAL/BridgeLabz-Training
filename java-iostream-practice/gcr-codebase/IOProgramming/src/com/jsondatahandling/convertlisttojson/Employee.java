17fd262e9e31fa63df621faf9e655202e877fc33
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-02-05 09:13:21 +0530

    [Ramvishal]: added program to convert list to json

---
+++ java-iostream-practice/gcr-codebase/IOProgramming/src/com/jsondatahandling/convertlisttojson/Employee.java
@@ -0,0 +1,28 @@
+package com.jsondatahandling.convertlisttojson;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+
+    public Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public Employee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
