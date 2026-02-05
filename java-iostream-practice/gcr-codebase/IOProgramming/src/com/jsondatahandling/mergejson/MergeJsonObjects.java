cc96d4236f3554c3ccb70ee0affa69f5a4bbaff7
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-02-05 09:17:53 +0530

    [Ramvishal]:added program to merge json files

---
+++ java-iostream-practice/gcr-codebase/IOProgramming/src/com/jsondatahandling/mergejson/MergeJsonObjects.java
@@ -0,0 +1,33 @@
+package com.jsondatahandling.mergejson;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+
+public class MergeJsonObjects {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            File file1 = new File("samplefiles/mergejson/user1.json");
+            File file2 = new File("samplefiles/mergejson/user2.json");
+
+            JsonNode node1 = mapper.readTree(file1);
+            JsonNode node2 = mapper.readTree(file2);
+
+            ObjectNode merged = mapper.createObjectNode();
+ 
+            merged.setAll((ObjectNode) node1);
+ 
+            merged.setAll((ObjectNode) node2);
+
+            System.out.println("Merged JSON:");
+            System.out.println(merged.toPrettyString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
