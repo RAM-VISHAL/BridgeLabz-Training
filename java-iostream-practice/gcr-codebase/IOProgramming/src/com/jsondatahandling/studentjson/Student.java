a3d614a35f3e4857b9e45afb9887aba963167096
RAM-VISHAL <ramvishalgonekar@gmail.com>
2026-02-05 09:21:03 +0530

    [Ramvishal]:added program of Student json

---
+++ java-iostream-practice/gcr-codebase/IOProgramming/src/com/jsondatahandling/studentjson/Student.java
@@ -0,0 +1,44 @@
+package com.jsondatahandling.studentjson;
+
+import java.util.List;
+
+public class Student {
+    private String name;
+    private int age;
+    private List<String> subjects;
+
+    public Student(String name, int age, List<String> subjects) {
+        this.name = name;
+        this.age = age;
+        this.subjects = subjects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', age=" + age + ", subjects=" + subjects + "}";
+    }
+}
